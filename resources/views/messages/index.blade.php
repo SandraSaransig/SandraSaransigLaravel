a763d6c3ab8671ef2b711db44f2c72aef0d6807f
SandraSaransig <sansarvac@alu.edu.gva.es>
2024-02-08 21:08:33 +0100

    mensajes arreglado, cambiar estilo para leido y no leido

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -1,21 +1,22 @@
 @extends('layout')
+
 @section('title','Mensajes')
+
 @section('content')
 <h1>Mensajes</h1>
 @forelse ($messages as $menssage)
-    @if ($menssage->readed)
-    {{$menssage->name}}
-
+    @if ($menssage->readed==0)
+        {{$menssage->name}}
     @endif
 
-<div class="mensaje">
+    <div class="mensaje">
 
 
 
-    <a href="{{route('messages.show', $menssage->id)}}">{{$menssage->name}}</a>
+        <a href="{{route('messages.show', $menssage->id)}}">{{$menssage->name}}</a>
 
 
-</div>
+    </div>
 
 @empty
 
